8419f23d21b14de992b44c47f79d4d9da86e8cae
Faysal598 <faysalmahmud735@gmail.com>
2023-01-05 10:31:17 -0800

    leet-code-practice-problems

--- LeetCode-Practice-Problems/1009. Complement of Base 10 Integer.cpp
+++ LeetCode-Practice-Problems/1009. Complement of Base 10 Integer.cpp
@@ -1,17 +1,53 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-int main()
+
+int bitwiseComplement(int n)
 {
-    int n;
-    cin >> n;
-    int i = 0;
-    int bin[32];
+    if (n == 0)
+        return 1;
+
+    int ans = 0, i = 0;
     while (n != 0)
     {
-        bin[i] = n % 10;
-        // cout << bin[i];
-        n = n / 10;
+        int bit = n & 1;
+        if (bit == 1)
+        {
+            bit = 0;
+        }
+        else if (bit == 0)
+        {
+            bit = 1;
+        }
+
+        ans = (bit * pow(10, i)) + ans;
+
+        n = n >> 1;
         i++;
     }
+
+    int sum = 0, j = 0;
+    while (ans != 0)
+    {
+        int digit = ans % 10;
+        if (digit == 1)
+        {
+            sum = sum + pow(2, j);
+        }
+
+        ans = ans / 10;
+        j++;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int n, result;
+    cin >> n;
+
+    cout << bitwiseComplement(n) << endl;
+
     return 0;
 }
